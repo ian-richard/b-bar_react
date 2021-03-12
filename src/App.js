a60192925b9f582e46700f67d300ed5824b1b882
Ian F <ian.fiddeman@gmail.com>
2021-03-12 21:56:18 +0000

    props and useState added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,37 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Harry', age: '25'},
+      { name: 'Jerry', age: '29'},
+      { name: 'Jane', age: '28'}
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log('was clicked');
+    this.setState({
+      persons: [
+        { name: 'Jules', age: '33'},
+        { name: 'Tina', age: '43'},
+        { name: 'Dana', age: '40'}
+      ]
+    }
+
+    )
+
+  }
+
   render() {
     return (
       <div className="App">
      <h1>Hi</h1>
-     <Person name='Harry' age='25'/>
-     <Person name='Jerry' age='29'/>
-     <Person name='Jane' age='28'/>
+     <button onClick={this.switchNameHandler}>Switch</button>
+     <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+     <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I like racing</Person>
+     <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+     
       </div>
     );
     // return React.createElement('div', null, 'h1', 'Hi I\'m a React App!');
